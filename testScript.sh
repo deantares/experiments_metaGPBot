3d8b2ceb417edc10be87fc140382ec2ebeafebf9
Antonio Fernández Ares <antares.es@gmail.com>
2015-06-26 13:31:19 +0200

    Añadido fichero de test para el algoritmo genético

---
+++ testScript.sh
@@ -0,0 +1,4 @@
+echo "[1.1822276084571166:[if(randomValue<0.218), if(actualMyShipsRatio<0.768), attackWeakestEnemyPlanet(0.727);, attackWealthestPlanet(0.669);, if(myShipsLandedFlyingRatio<0.312), doNothing(0.233);, 
+attackQuickEnemyPlanet(0.809);]]
+45898"
+
